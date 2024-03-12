f428e855c85f7f29339771a16146703ed953d361
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2024-03-12 11:36:01 +0000

    Create and add service worker

---
+++ index.js
@@ -0,0 +1,39 @@
+import { trackPageview, trackEvent } from "./src/analytics-api.js"
+
+// Your code here
+
+// Add function on the sign up button independetly of how page is set up
+// Use href="/signup"
+
+// Innialise indexedDB to let uniquily identify users
+
+// Of active pages display randomly the page, and make sure all api
+// Requests are linked to that page
+
+// TODO: Randomly select page for user
+// TODO: Don't change page when refreshed
+
+// TODO: link track api request to signup button
+// TODO: link track api requests to view site
+
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      })
+      if (registration.installing) {
+        console.log("Service worker installing")
+      } else if (registration.waiting) {
+        console.log("Service worker installed")
+      } else if (registration.active) {
+        console.log("Service worker active")
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`)
+    }
+  }
+}
+
+
+registerServiceWorker()
