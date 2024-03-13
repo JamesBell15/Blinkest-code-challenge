1bba184421f65c264de7b59353846d10b11abd82
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2024-03-13 20:40:01 +0000

    Clean up index.js

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-
-// Your code here
-
-// Add function on the sign up button independetly of how page is set up
-// Use href="/signup"
-
-// Innialise indexedDB to let uniquily identify users
-
-// Of active pages display randomly the page, and make sure all api
-// Requests are linked to that page
-
-// TODO: Randomly select page for user
-// TODO: Don't change page when refreshed
-
-// TODO: link track api request to signup button
-// TODO: link track api requests to view site
+import { EXPERIMENTS } from "./src/experiments.js";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-// No access to the file system so add manually
-const experiments = [
-  "/src/experiments/a.html",
-  "/src/experiments/b.html",
-]
-
+// Randomly assigns a page to the user and stores page
 let test = localStorage.getItem("src");
 
 if (!test) {
-  let tests = experiments.filter((word) => word.includes('/src/experiments/'))
+  let tests = EXPERIMENTS.filter((word) => word.includes('/src/experiments/'))
 
   test = tests[getRandomInt(tests.length)]
 
