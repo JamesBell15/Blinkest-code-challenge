dc4d5626fa0bb1f894e52de663c63964bd13ef3c
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2024-03-13 18:24:00 +0000

    Update api calls to send data

--- src/analytics-api.js
+++ src/analytics-api.js
@@ -3,8 +3,18 @@
  * Send as params whatever you might seem valuable to send.
  * The URL is probably a good start though.
  */
-export const trackPageview = (params) => {
+export const trackPageview = async (params) => {
   console.log(`--> Tracking Pageview: ${params}`);
+
+  let pageView = localStorage.getItem("page-view");
+
+  if (!pageView) {
+    let time = Date.now()
+
+    localStorage.setItem("page-view", time);
+
+    apiEndPoint({type: "page-view", time: time})
+  }
 };
 
 /**
@@ -12,6 +22,62 @@ export const trackPageview = (params) => {
  * Send as params whatever you might seem valuable to send.
  * The URL and an event name are probably a good start though.
  */
-export const trackEvent = (params) => {
+export const trackEvent = async (params) => {
   console.log(`--> Tracking Event: ${params}`);
+
+  let signupClick = localStorage.getItem("signup-click");
+
+  if (!signupClick) {
+    let time = Date.now()
+
+    localStorage.setItem("signup-click", time);
+
+    apiEndPoint({type: "signup-click", time: time})
+  }
 };
+
+const apiEndPoint = async (params) => {
+  let data = {}
+
+  if (params["type"] == "page-view") {
+    data = { time: params["time"] }
+
+    const requestIDB = indexedDB.open("db", 4)
+
+    requestIDB.onsuccess = async (event) => {
+      const transaction = requestIDB.result.transaction(["pageViews"], "readwrite")
+      const sightingStore = transaction.objectStore("pageViews")
+
+      sightingStore.add(data).onsuccess = async (event) => {
+        console.log("page view added")
+      }
+    }
+  }
+
+  if (params["type"] == "signup-click") {
+    data = { time: params["time"] }
+
+    const requestIDB = indexedDB.open("db", 4)
+
+    requestIDB.onsuccess = async (event) => {
+      const transaction = requestIDB.result.transaction(["signupClicks"], "readwrite")
+      const sightingStore = transaction.objectStore("signupClicks")
+
+      sightingStore.add(data).onsuccess = async (event) => {
+        console.log("sign up click added")
+      }
+    }
+
+  }
+}
+
+window.onload = async function () {
+
+  trackPageview()
+
+  let link = document.getElementById("signup");
+
+  link.onclick = function signupClick() {
+    trackEvent()
+  }
+}
